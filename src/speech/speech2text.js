47adb2de0c2d16008ee51bce0c5ea4efd77f1360
Ttayu <ykisoccer.pg@gmail.com>
2018-10-21 11:22:35 +0900

    Use async implementation

--- src/speech/speech2text.js
+++ src/speech/speech2text.js
@@ -14,43 +14,48 @@ const languageCode = 'ja-JP';
 // Detects speech in the audio file
 // fileName: xxx.wav
 // writeFileName: xxx.txt
-function speech2text(fileName, writeFileName) {
-    // Reads a local audio file and converts it to base64
-    const file = fs.readFileSync(fileName);
-    const audioBytes = file.toString('base64');
+const speech2text = (fileName) => {
+    return new Promise((resolve) => {
+        // Reads a local audio file and converts it to base64
+        const file = fs.readFileSync(fileName);
+        const audioBytes = file.toString('base64');
 
-    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-    const audio = {
-        content: audioBytes,
-    };
-    const config = {
-        encoding: encoding,
-        sampleRateHertz: sampleRateHertz,
-        languageCode: languageCode,
-    };
-    const request = {
-        audio: audio,
-        config: config,
-    };
-    client
-        .recognize(request)
-        .then(data => {
-            const response = data[0];
-            const transcription = response.results
-                .map(result => result.alternatives[0].transcript)
-                .join('\n');
-            fs.writeFile(writeFileName, transcription, err => {
-                if (err) {
-                    console.error('ERROR:', err);
-                    return;
-                }
-                console.log(`Text content written to file: ${writeFileName}`);
+        // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+        const audio = {
+            content: audioBytes,
+        };
+        const config = {
+            encoding: encoding,
+            sampleRateHertz: sampleRateHertz,
+            languageCode: languageCode,
+        };
+        const request = {
+            audio: audio,
+            config: config,
+        };
+        let transcription = ''
+        client
+            .recognize(request)
+            .then(data => {
+                const response = data[0];
+                transcription = response.results
+                    .map(result => result.alternatives[0].transcript)
+                    .join('\n');
+                console.log(`Transcription: ${transcription}`);
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
             });
-            console.log(`Transcription: ${transcription}`);
-        })
-        .catch(err => {
-            console.error('ERROR:', err);
-        });
+        resolve(transcription)
+    })
 }
 
-exports.speech2text = speech2text;
+speech2text('../../resources/askyou.wav')
+    // .then((transcription) => {
+    //     console.log('aaa', transcription)
+    // })
+    .catch((err) => {
+        console.error('ERROR:', err);
+    });
+
+// exports.speech2text = speech2text;
