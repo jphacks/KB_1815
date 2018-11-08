44b394145ae0ef311ac69be83a34f3de48b7af36
Ttayu <ykisoccer.pg@gmail.com>
2018-11-09 07:32:01 +0900

    update

--- src/switch.js
+++ src/switch.js
@@ -8,14 +8,14 @@ require('date-utils');
 const childProcess = require('child_process');
 
 //使用したGPIOポートの開放
-var cleanUp = function() {
-　　　　for (var i=0; i < PIN_LIST.length; i++) {
+var cleanUp = function () {
+        for (var i = 0; i < PIN_LIST.length; i++) {
                 fs.writeFileSync(GPIO_DIR + '/unexport', PIN_LIST[i]);
         }
 };
 
 //使用するGPIOポートの準備設定
-var setUp = function(pin, io) {
+var setUp = function (pin, io) {
         try {
                 fs.writeFileSync(GPIO_DIR + '/export', pin);
                 PIN_LIST.push(pin);
@@ -27,7 +27,7 @@ var setUp = function(pin, io) {
 };
 
 //GPIOの入力状態を取得
-var getInput = function(pin) {
+var getInput = function (pin) {
         try {
                 var cntxt = fs.readFileSync(GPIO_PIN_DIR + pin + '/value');
                 return cntxt.toString().split('\n')[0];
@@ -38,7 +38,7 @@ var getInput = function(pin) {
 };
 
 //GPIOの出力状態を設定
-var setOutput = function(pin, value) {
+var setOutput = function (pin, value) {
         try {
                 fs.writeFileSync(GPIO_PIN_DIR + pin + '/value', value);
         } catch (e) {
@@ -56,43 +56,45 @@ try {
         //0.01毎にGPIO 7の入力状態を確認し、
         //電流が流れている(スイッチが押されている)場合
         //LEDを点灯させる。
-	let count = 0;
-        var moniter = setInterval(function() {
+        let pushedButton = false;
+        var moniter = setInterval(function () {
                 var value = getInput(7);
                 if (value == '1') {
                         //スイッチが押された状態
- 			count += 1;
                         setOutput(8, '1');
-			if (count === 1) {
-		          childProcess.exec('aplay -D plughw:1,0 public/resources/pinpon.wav') 
-                          setTimeout(() => {
-			  childProcess.exec('aplay -D plughw:1,0 ../resources/start.wav')}, 2000);
-			  // const dt = new Date();
-                          // const formatted = dt.toFormat("YYYYMMDDHH24MISS");
-                          childProcess.exec(`fswebcam ./public/images/comehome.jpg`, (error, stdout, stderr) => {
-                          if(error) {
-                            console.log(stderr);
-                            return;
-                          }
-                          else {
-                           console.log("success");
-                          }
-                        });
-
-                        const options = {
-                          uri: "https://uketori.herokuapp.com/notify",
-                          headers: {
-                            "Content-Type": "application/json"
-                          },
-                          json: {
-                            "result": "comehome.jpg"
-                          }
-                        };
- 			request.post(options, function (error, response, body) {
-                          console.log('ok')
-                        });}
-                        } else {
+                        if (pushedButton === false) {
+                                childProcess.exec('aplay -D plughw:1,0 public/resources/pinpon.wav')
+                                setTimeout(() => {
+                                        childProcess.exec('aplay -D plughw:1,0 ../resources/start.wav')
+                                }, 2000);
+                                // const dt = new Date();
+                                // const formatted = dt.toFormat("YYYYMMDDHH24MISS");
+                                childProcess.exec(`fswebcam ./public/images/comehome.jpg`, (error, stdout, stderr) => {
+                                        if (error) {
+                                                console.log(stderr);
+                                                return;
+                                        }
+                                        else {
+                                                console.log("success");
+                                        }
+                                });
+                                const options = {
+                                        uri: "https://uketori.herokuapp.com/notify",
+                                        headers: {
+                                                "Content-Type": "application/json"
+                                        },
+                                        json: {
+                                                "result": "comehome.jpg"
+                                        }
+                                };
+                                request.post(options, function (error, response, body) {
+                                        console.log('ok')
+                                });
+                                pushedButton = true
+                        }
+                } else {
                         //スイッチが離された状態
+                        pushedButton = false
                         setOutput(8, '0');
                 }
         }, 10);
@@ -101,12 +103,12 @@ try {
         cleanUp();
 }
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
         cleanUp();
         process.exit(0);
 });
 
-process.on('end', function() {
+process.on('end', function () {
         cleanUp();
         process.exit(0);
 });
